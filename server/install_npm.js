9eb24dbedf29cb8f95290dd73df1896999d010fd
ashuisalluneed <zadavashish1995@gmail.com>
2026-04-02 21:46:45 +0530

    chore: remove unused files and setup scripts

--- server/install_npm.js
+++
@@ -1,2 +0,0 @@
-import { execSync } from "child_process";
-execSync("npm install", { stdio: "inherit" });
--- server/install_stripe.js
+++
@@ -1,7 +0,0 @@
-import { execSync } from "child_process";
-try {
-  execSync("npm install stripe", { stdio: "inherit" });
-  console.log("Successfully installed stripe");
-} catch (e) {
-  console.error("Failed to install stripe", e);
-}
